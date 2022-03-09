c3e2f5e2b9c20ee5adbfa218cd63bb8005054d3a
Shaneeyb <Shaneburkework@gmail.com>
2022-03-08 14:36:14 -1000

    first test

--- src/App.js
+++ src/App.js
@@ -7,13 +7,10 @@ import Main from './components/MainComponent';
 function App() {
 return (
 	<div style={{
-		backgroundImage: `url("https://images.unsplash.com/photo-1470549584009-d347338fc0ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1888&q=80")`, 
-		backgroundPosition: 'bottom',
-		backgroundAttachment: "fixed",
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        width: '100vw',
-        height: 'auto'
+		backgroundColor: "#FFFAF3",
+		color: "#2D2D2D",
+        height: '100vh',
+  		minHeight : '100vh'
 	}}>
 		<Main />
 	</div>
